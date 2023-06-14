c3f44bfacb51c2fc3ce3f7926698fc4ca6264344
Corey Vesey <vesey8507@gmail.com>
2023-06-13 18:37:46 -0700

    fixed routing issues

--- models/thought.js
+++ models/thought.js
@@ -1,25 +1,5 @@
 const mongoose = require("mongoose");
 
-const thoughtSchema = new mongoose.Schema({
-    thoughtText: {
-        type: String,
-        required: true,
-        maxLength: 280,
-        minLength: 1
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    },
-    username: {
-        type: String,
-        required: true
-    },
-    reactions: [
-        reactionSchema
-    ]
-});
-
 const reactionSchema = new mongoose.Schema({
     reactionId: {
         type: mongoose.Schema.Types.ObjectId,
@@ -39,6 +19,26 @@ const reactionSchema = new mongoose.Schema({
         type: Date,
         default: Date.now()
     }
+});
+
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        maxLength: 280,
+        minLength: 1
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [
+        reactionSchema
+    ]
 });
 
 thoughtSchema.virtual("reactionCount").get(function() {
